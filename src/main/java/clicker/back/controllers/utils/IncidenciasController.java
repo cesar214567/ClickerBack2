cfbf436a6071eed4b0ebfcf7b594fd5b5dd21679
cesar <cesar.madera@utec.edu.pe>
2021-06-27 18:16:48 -0500

    push server

--- src/main/java/clicker/back/controllers/utils/IncidenciasController.java
+++ src/main/java/clicker/back/controllers/utils/IncidenciasController.java
@@ -8,12 +8,17 @@ import clicker.back.utils.entities.Incidencias;
 import clicker.back.utils.errors.ResponseService;
 import clicker.back.utils.services.IncidenciasService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @RestController
@@ -42,8 +47,18 @@ public class IncidenciasController {
 
     @PostMapping
     @ResponseBody
-    public ResponseEntity<Object> save(@RequestPart("incidencias") String model, @RequestPart("file")MultipartFile file ){
+    public ResponseEntity<Object> save(HttpServletRequest request){
         try{
+            MultipartFile file = null;
+            String model="{}";
+            for (Part part:request.getParts()){
+                if(part.getContentType()!=null ){
+                    file = new MockMultipartFile(part.getSubmittedFileName(),part.getSubmittedFileName(),part.getContentType(),part.getInputStream());
+                }else{
+                    String theString = IOUtils.toString(part.getInputStream(), StandardCharsets.UTF_8);
+                    model = String.valueOf(theString);
+                }
+            }
             ObjectMapper objectMapper = new ObjectMapper();
             Incidencias incidencias = objectMapper.readValue(model,Incidencias.class);
 
@@ -56,12 +71,12 @@ public class IncidenciasController {
             incidencias.setUsuario(usuariosService.getById(incidencias.getUsuario().getId()));
             incidencias.setId(null);
             incidencias.setDate(new Date());
-            incidencias = incidenciasService.save(incidencias);
             if (file!=null){
                 incidencias.setFoto(amazonService.uploadFile(file,incidencias.getUsuario().getId().toString(),"fotosIncidencias/"+incidencias.getId()));
             }else{
                 incidencias.setFoto(null);
             }
+            incidencias = incidenciasService.save(incidencias);
             return ResponseService.genSuccess(incidencias);
         }catch (Exception e){
             e.printStackTrace();
